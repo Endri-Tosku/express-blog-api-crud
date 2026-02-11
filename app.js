283bbd1a266d10996ba0cc35d8a40e91ec2b78a2
Endri-Tosku <endritosku001@gmail.com>
2026-02-11 17:24:09 +0100

    feat: aggiunto middleware delle rotte non registrate

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// importo il middelware 
+// importo il middelware errori
+const notFound = require("./middlewares/notFound")
+
+// importo il middelware errori
 const errorsHandler = require("./middlewares/errorsHandler")
 
 // importiamo le rotte dei post
@@ -19,6 +22,10 @@ app.get("/", (req, res) => {
 // colleghiamo le rotte dei post
 app.use('/posts', postRoutes);
 
+// middleware per endpoint non trovati
+app.use(notFound);
+
+// middleware globale per gestione errori
 app.use(errorsHandler);
 
 app.listen(port, () => {
